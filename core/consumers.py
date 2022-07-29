efca6a87186763bb77067b4ed40ed7abc75d9ba4
Akshay Das <63033525+akshay98322@users.noreply.github.com>
2022-07-29 18:13:27 +0530

    Added DB

--- core/consumers.py
+++ core/consumers.py
@@ -1,9 +1,9 @@
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-# from channels.exceptions import StopConsumer
+from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
 import json
-# from .models import Group, Chat
-# from channels.db import database_sync_to_async
+from .models import Group, Chat
+from channels.db import database_sync_to_async
 
 
 # WebsocketConsumer
@@ -16,7 +16,11 @@ class MyWebsocketConsumer(WebsocketConsumer):
     
     def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
+        # find the group
+        group = Group.objects.get(name=self.group_name)
+        # save chat
         message = data['msg']
+        chat = Chat.objects.create(content=message, group=group)
         async_to_sync(self.channel_layer.group_send)(self.group_name, {
                 'type': 'chat.message',
                 'message': message
@@ -45,7 +49,11 @@ class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
     
     async def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
+        # find the group
+        group = await database_sync_to_async(Group.objects.get)(name=self.group_name)
         message = data['msg']
+        # create a chat
+        chat = await database_sync_to_async(Chat.objects.create)(content=message, group=group)
         await self.channel_layer.group_send(self.group_name, {
                 'type': 'chat.message',
                 'message': message
