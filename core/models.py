8c96e318c9b8edb878c971072e3fd3f51f44145a
Akshay Das <63033525+akshay98322@users.noreply.github.com>
2022-07-30 18:32:29 +0530

    UI Updated

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Chat(models.Model):
     content  = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now=True)
     group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
