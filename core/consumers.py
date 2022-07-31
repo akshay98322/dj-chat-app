78e1e30ff4f764e60a5a784bda0f7d7795b80f82
Akshay Das <63033525+akshay98322@users.noreply.github.com>
2022-07-31 15:50:01 +0530

    Added  JsonWebsocketConsumer

--- core/consumers.py
+++ core/consumers.py
@@ -1,4 +1,4 @@
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer, JsonWebsocketConsumer, AsyncJsonWebsocketConsumer
 from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
 import json
@@ -91,4 +91,45 @@ class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
         print("disconnected", close_code)
         raise StopConsumer()
-     
+
+# JsonWebsocketConsumer
+class MyJsonWebsocketConsumer(JsonWebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.group_name = self.scope['url_route']['kwargs']['gname']
+        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
+        print("connected")
+    
+    def receive_json(self, content, **kwargs):
+        if self.scope['user'].is_authenticated:
+            # find the group
+            group = Group.objects.get(name=self.group_name)
+            # get user
+            u_name = self.scope['user'].username
+            user_obj = User.objects.get(username=u_name)
+            # save chat
+            message = content['msg']
+            chat = Chat.objects.create(content=message, group=group, user=user_obj)
+            async_to_sync(self.channel_layer.group_send)(self.group_name, {
+                    'type': 'chat.message',
+                    'message': message,
+                    'user': u_name
+                })        
+        else:
+            self.send(text_data=json.dumps({
+            'msg':'You are not authenticated.',
+            'user':'Anonymous'
+            }))
+            raise StopConsumer()
+
+    # handeller function
+    def chat_message(self, event):
+        self.send_json({
+            'msg': event['message'],
+            'user': event['user']
+        })
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(self.group_name, self.channel_name)
+        print("disconnected", close_code)
+        raise StopConsumer()
--- core/routing.py
+++ core/routing.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
+    # wessocket consumers
     path('ws/wsc/<str:gname>/', consumers.MyWebsocketConsumer.as_asgi()),
     path('ws/awsc/<str:gname>/', consumers.MyAsyncWebsocketConsumer.as_asgi()),
+    # json websocket consumers
+    path('ws/jwc/<str:gname>/', consumers.MyJsonWebsocketConsumer.as_asgi()),
+
 ]
